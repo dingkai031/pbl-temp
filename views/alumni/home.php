0d46d2c5ab5d719e63804f247ec0a5a7d6bf3515
Yovan Julio Adam <yovanjulioadam@gmail.com>
2023-06-23 19:36:53 +0700

    update alumni home

--- views/alumni/home.php
+++ views/alumni/home.php
@@ -19,48 +19,49 @@
         </div>
         <div class="home">
             <?php echo "Hallo " . $_SESSION['username'] . "!" . " Selamat Datang di Tracer Alumni"; ?>  <br>
+            <pre>
+                <?php print_r($data['alumni_data']) ?>
+            </pre>
             <table class="table">
             <tbody>
                     <?php
-                    include "koneksi.php";
-                    $query = mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE id_mahasiswa");
-                    $no = 1;
-                    while ($data = mysqli_fetch_array($query)) {
-                        // Check if the current row's data matches the logged-in user
-                    if ($_SESSION['id_mahasiswa'] == $data['id_mahasiswa']) {
+                    
+                    // while ($data = mysqli_fetch_array($query)) {
+                    //     // Check if the current row's data matches the logged-in user
+                    // if ($_SESSION['id_mahasiswa'] == $data['id_mahasiswa']) {
                     ?>
                     <tr>
                         <th>NIM :</th>
-                        <td><?php echo $data['nim']; ?></td>                    
+                        <td><?php //echo $data['nim']; ?></td>                    
                     </tr>
                     <tr>
                         <th>Nama :</th>
-                        <td><?php echo $data['nama_lengkap']; ?></td>
+                        <td><?php //echo $data['nama_lengkap']; ?></td>
                     </tr>
                     <tr>
                         <th>Prodi :</th>
-                        <td><?php echo $data['prodi']; ?></td>
+                        <td><?php //echo $data['prodi']; ?></td>
                     </tr>
                     <tr>
                         <th>Email :</th>
-                        <td><?php echo $data['email_kampus']; ?></td>
+                        <td><?php //echo $data['email_kampus']; ?></td>
                     </tr>
                     <tr>
                         <th>Telp :</th>
-                        <td><?php echo $data['telp']; ?></td>
+                        <td><?php //echo $data['telp']; ?></td>
                     </tr>
                     <tr>
                         <th>Jenis Kelamin :</th>
-                        <td><?php echo $data['jenis_kelamin']; ?></td>
+                        <td><?php //echo $data['jenis_kelamin']; ?></td>
                     </tr>
                     <tr>
                         <th>IPK :</th>
-                        <td><?php echo $data['ipk']; ?></td>
+                        <td><?php //echo $data['ipk']; ?></td>
                     </tr>
                     <!-- Add the necessary code to display other columns from the "mahasiswa" table -->                
                     <?php
-                        }
-                    }
+                    //     }
+                    // }
                     ?>
             </tbody>
             </table>
