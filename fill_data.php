49021ff3b78f61d3307af4ee20b51ef264256c0b
Yovan Julio Adam <yovanjulioadam@gmail.com>
2023-06-24 15:27:45 +0700

    fill data

--- fill_data.php
+++ fill_data.php
@@ -2,9 +2,33 @@
 
 include_once "helpers.php";
 
+// untuk masukin user perusahaan
 
-$query = "INSERT INTO user (id_mahasiswa, username, full_name, password, email, created_at, updated_at) VALUES ('1', 'usersatu', 'user satu', '".password_hash('usersatu', PASSWORD_BCRYPT)."', 'usersatu@gmail.com', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
-mysqli_query($koneksi, $query);
+$id_perusahaan = ""; //id_perusahaan, masukin dulu secara manual di database
+$username = ""; //test008
+$password = ""; //password031
+$email= ""; //test@gmail.como
+$level= "2"; 
+
+$query = "INSERT INTO user (level, id_perusahaan, username, password, email, created_at, updated_at) VALUES ('$level','$id_perusahaan', '$username', '".password_hash($password, PASSWORD_BCRYPT)."', '$email', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
+if (mysqli_query($koneksi, $query)) {
+    echo "user $username berhasil ditambahkan";
+}else {
+    echo "error ".mysqli_error($koneksi);
+}
+
+// ===============================untuk masukin admin
+// $username = ""; //test008
+// $password = ""; //password031
+// $email= ""; //test@gmail.como
+// $level= "1";
+
+// $query = "INSERT INTO user (level, username, password, email, created_at, updated_at) VALUES ('$level', '$username', '".password_hash($password, PASSWORD_BCRYPT)."', '$email', '".date('Y-m-d H:i:s')."', '".date('Y-m-d H:i:s')."')";
+// if (mysqli_query($koneksi, $query)) {
+//     echo "user $username berhasil ditambahkan";
+// }else {
+//     echo "error ".mysqli_error($koneksi);
+// }
 
 
 ?>
